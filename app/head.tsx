c35c90ab1a397f1385f1f19d1a68c6513ffe0ebf
viewsonic2023 <andres.garrido@bairesdev.com>
2025-06-26 15:58:41 -0500

    15h58

--- app/head.tsx
+++ app/head.tsx
@@ -1,16 +1,20 @@
 export default function Head() {
-    return (
-      <>
-        <title>My Simple App</title>
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]);t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),e+"."+t},p=["capture","register","register_once","unregister","identify","alias","add_payment_method","set_distinct_id","clear_charges","set_person_properties","capture_pageview","reset","track_charge","track_pageview","track_links","track_forms","on","off","once","unsubscribe","capture_screen","on_click","bind_register_for_event","track_pageview_with_metadata","track_link_clicked","track_event","track_autocapture","track_click","track_submit","track_change","track_focus","track_blur","track_elements","track_exit","track_visibility","track_email_opened","track_email_clicked","track_sms_clicked","track_cta_clicked","onFeatureFlags","getFeatureFlag","isFeatureEnabled","getFeatureFlagPayload","reloadFeatureFlags","groupIdentify","resetGroups","setPersonPropertiesForFlags","resetPersonPropertiesForFlags"],i=0;i<p.length;i++)g(u,p[i]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-              posthog.init('phc_rpkEqRKayqjT2v86H1hpwhJguVUZkchiWdXpSixTAPl', {api_host: 'https://app.posthog.com'})
-            `,
-          }}
-        />
-      </>
-    )
-  }
+  return (
+    <>
+      <title>My Simple App</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init Ie Ts Ms Ee Es Rs capture Ge calculateEventProperties Os register register_once register_for_session unregister unregister_for_session js getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSurveysLoaded onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey canRenderSurveyAsync identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty Ds Fs createPersonProfile Ls Ps opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing clear_opt_in_out_capturing Cs debug I As getPageViewId captureTraceFeedback captureTraceMetric".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+          posthog.init('phc_Ebs3JJiGWuW1iybzhB14IXmuQ4miCELoP9V6RflUjKb', {
+              api_host: 'https://us.i.posthog.com',
+              defaults: '2025-05-24',
+              person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+          })
+          `,
+        }}
+      />
+    </>
+  )
+}
